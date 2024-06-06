1d40da3779fcbcfec06f33f3128750004050b1b8
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-07 01:23:28 +0300

    a type-annotated function floor which takes a float n as argument and returns the floor of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains the function floor which takes a float n as argument
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    This function returns the floor of the float n
+    """
+    return math.floor(n)
