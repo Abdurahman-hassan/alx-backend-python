26f1e503831bf55104de20227225ff18cfa69d79
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-07 01:55:37 +0300

    fix linting

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,6 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Type-annotated function element_length that
+    takes a list input_list of string as argument and returns a list"""
     return [(i, len(i)) for i in lst]
