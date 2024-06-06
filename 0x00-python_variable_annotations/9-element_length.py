ed77f29b6cc215c8ca64c495ef1ce2635073d31d
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-07 01:49:29 +0300

    Annotate the below function’s parameters and return values with the appropriate types
    def element_length(lst):
        return [(i, len(i)) for i in lst]

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Type-annotated function element_length that
+    takes a list input_list of string as argument and returns a list"""
+from typing import List
+
+
+def element_length(lst: List[str]) -> List[int]:
+    """Return a list of integers representing
+        the lengths of the strings in lst"""
+    return [len(x) for x in lst]
