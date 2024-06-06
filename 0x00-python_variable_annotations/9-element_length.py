3892254a5b270f82e2e4fb6753a3049bb00352ea
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-07 01:54:24 +0300

    make function return {'lst': typing.Iterable[typing.Sequence], 'return': typing.List[typing.Tuple[typing.Sequence, int]]}

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""task 9 module"""
-from typing import List
+"""Type-annotated function element_length that
+    takes a list input_list of string as argument and returns a list"""
+
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[int]:
-    """Return a list of integers representing
-        the lengths of the strings in lst"""
-    return [len(x) for x in lst]
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
