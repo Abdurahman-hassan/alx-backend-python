6197696d545775693179a49ccff795ebaaaadce3
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-07 01:26:00 +0300

    fix linting

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""Module that takes a float n and returns the string representation of the float"""
+"""Module that takes a float n and
+    returns the string representation of the float"""
 
 
 def to_str(n: float) -> str:
-    """Function that takes a float n and returns the string representation of the float"""
+    """Function that takes a float n
+        and returns the string representation of the float"""
     return str(n)
